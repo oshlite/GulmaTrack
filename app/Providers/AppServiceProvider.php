700e5c65330cc3e00f43080179447b1a3df2e84d
oshlite <oryza1412@gmail.com>
2026-03-10 05:55:22 +0700

    force https untuk local environment dengan cloudflared tunnel bismillah seminar ayo ayo ayo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         // Force HTTPS untuk local environment saat pakai ngrok
         if (app()->environment('local')) {
-            URL::forceScheme('http');
+            URL::forceScheme('https');
         }
     }
 }
